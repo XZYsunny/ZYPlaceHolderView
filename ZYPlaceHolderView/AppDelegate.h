6eaaa7d9052446653c3bc15af1c91da1475e502f
sunny <sunnyyangzx@sina.com>
2017-03-15 13:49:55 +0800

    Initial Commit

---
+++ ZYPlaceHolderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYPlaceHolderView
+//
+//  Created by sunny on 2017/3/15.
+//  Copyright © 2017年 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
